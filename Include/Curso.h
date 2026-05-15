e6c6a73d5e8be81e1c2ea29ab2401dc028c32d9b
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 17:59:13 -0500

    commit curso.h final commit matricula h inicial

--- Include/Curso.h
+++ Include/Curso.h
@@ -22,5 +22,57 @@ class Course {
     public:
 
     Course (int coursecode, std::string coursename, int credits);
+
+     //Getters 
+
+     /**
+     * @brief Obtiene el codigo del curso
+     * @return codigo del curso
+     */
+
+    int getCoursecode();
+
+
+     /**
+     * @brief Obtiene el nombre del curso
+     * @return nombre del curso
+     */
+
+    std::string getCoursename();
+
+
+    /**
+     * @brief Obtiene los creditos del curso
+     * @return creditos del curso
+     */
+
+    int getCredits();
+
+    //Setters
+
+      /**
+     * @brief Modifica el codigo del curso
+     * @param coursecode Nuevo codigo del curso
+     */
+
+     void setCoursecode( int coursecode);
+
+
+    /**
+     * @brief Modifica el nombre del curso
+     * @param nombreCurso Nuevo nombre del curso
+     */
+
+    void setCoursename( std::string coursename);
+
+       /**
+     * @brief Modifica los creditos del curso
+     * @param creditos Nueva cantidad de creditos
+     */
+
+    void setCredits( int credits);
+
+       void mostrarInformacion();
 };
+
 #endif
---
+++ Include/Matricula.h
@@ -0,0 +1,9 @@
+#ifndef MATRICULA_H
+#define MATRICULA_H
+
+#include <iostream>
+#include <string>
+#include "Estudiante.h"
+#include "Curso.h"
+
+#endif
