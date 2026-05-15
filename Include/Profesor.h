e6eafc087a45352431e927b3f22f3b78a88bb41b
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 17:00:12 -0500

    commit Mofied Ptofesor.h

--- Include/Profesor.h
+++ Include/Profesor.h
@@ -18,13 +18,42 @@ class Teacher : public Person{
     public:
 
     // Constructor
-    Teacher(std::string Name,
-             std::string Lastname,
-             int Age,
-             int Document,
-             std::string Specialty,
-             double Salary);
+    Teacher(std::string Name, std::string Lastname,int Age, int Document, std::string Specialty, double Salary);
 
+    //Getters 
+
+    /**
+     * @brief Obtiene la Specialty del profesor
+     * @return Specialty del profesor
+     */
+    std::string getSpecialty();
+
+     /**
+     * @brief Obtiene el Salary del profesor
+     * @return Salary del profesor
+     */
+   
+     double getSalary();
+
+    //Setters  
+
+      /**
+     * @brief Modifica la Specialty del profesor
+     * @param Specialty Nueva Specialty
+     */
+     void setSpecialty(std::string Specialty);
+
+      /**
+     * @brief Modifica el salary del profesor
+     * @param salary Nuevo salary
+     */
+     void setSalary(double salary);
+
+     // Metodo propio
+     void asignarCurso();
+
+     // Polimorfismo
+      void mostrarInformacion() override;
 
 };
 #endif
