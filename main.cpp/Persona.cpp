448e6d533ddcd732df47a9d404723087d73b7f75
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 19:11:11 -0500

    commit Persona.cpp

---
+++ main.cpp/Persona.cpp
@@ -0,0 +1,35 @@
+#include "../include/Persona.h"
+
+Person::Person(std::string Name, std::string Lastname, int Age, int Document) {
+    this->Name = Name;
+    this->Lastname = Lastname;
+    this->Age = Age;
+    this->Document = Document;
+}
+
+Person::~Person(){
+}
+
+//setters
+
+void Person::setName(std::string Name){
+    this->Name=Name;
+}
+void Person::setLastname(std::string Lastname){
+    this->Lastname=Lastname;
+}
+void Person::setDocument(int Document){
+    this->Document=Document;
+}
+
+//Getters
+
+std::string Person::getname(){
+    return Name=Name;
+}
+std::string Person::getLastname(){
+    return Lastname=Lastname;
+}
+int Person::getDocument(){
+    return Document=Document;
+}
