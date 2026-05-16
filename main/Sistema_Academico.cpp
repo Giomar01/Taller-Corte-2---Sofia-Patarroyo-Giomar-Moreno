6a3a007f671d5cd60a72a224a0c99df2174d02be
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 21:29:53 -0500

    commit sistema academico final 2

--- main/Sistema_Academico.cpp
+++ main/Sistema_Academico.cpp
@@ -32,9 +32,9 @@ int main (){
         std::cin >> opcion;
 
         switch(opcion) {
-            case 1:
-                std::string Name, Lastname, Code;
-            int Age, Document, Semester;
+            case 1: {
+                std::string Name, Lastname;
+            int Age, Document, Semester, Code;
             double Average;
 
             std::cout << "Nombre: ";
@@ -53,19 +53,113 @@ int main (){
             std::cin >> Average;
 
             Students[estudianteCount] =
-                new (name, lastname, age, document, code, semester, average);
+                new Student(Name, Lastname, Age, Document, Code, Semester, Average);
 
             estudianteCount++;
                 break;
-            case 2:
+            }
+            case 2:{
+
+            std::string Name, Lastname, Specialty;
+            int Age, Document;
+            double Salary;
+
+            std::cout << "Nombre: ";
+            std::cin >> Name;
+            std::cout << "Apellido: ";
+            std::cin >> Lastname;
+            std::cout << "Edad: ";
+            std::cin >> Age;
+            std::cout << "Documento: ";
+            std::cin >> Document;
+            std::cout << "Especialidad: ";
+            std::cin >> Specialty;
+            std::cout << "Salario: ";
+            std::cin >> Salary;
+
+            Teachers[profesorCount] =
+                new Teacher(Name, Lastname, Age, Document, Specialty, Salary);
+
+            profesorCount++;
+            
                 
                 break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
+            }
+            case 3:{
+            int Code, Credits;
+            std::string Name;
+
+            std::cout << "Codigo curso: ";
+            std::cin >> Code;
+            std::cout << "Nombre curso: ";
+            std::cin >> Name;
+            std::cout << "Creditos: ";
+            std::cin >> Credits;
+
+            Courses[cursoCount] = new Course(Code, Name, Credits);
+
+            cursoCount++;
+            break;
+            }
+            case 4:{
+
+            if (estudianteCount == 0 || cursoCount == 0) {
+                std::cout << "No hay estudiantes o cursos registrados.\n";
+                continue;
+            }
+
+            int estIndex, curIndex;
+
+            std::cout << "Indice estudiante (0 - " << estudianteCount - 1 << "): ";
+            std::cin >> estIndex;
+
+            std::cout << "Indice curso (0 - " << cursoCount - 1 << "): ";
+            std::cin >> curIndex;
+
+            Tuitions[matriculaCount] =
+                new Tuition(Students[estIndex], Courses[curIndex]);
+
+            matriculaCount++;
+            break;
+            }
+            case 5:{
+            for (int i = 0; i < estudianteCount; i++) {
+            std::cout << "\n[" << i << "] ";
+            Students[i]->mostrarInformacion();
+            }
+             break;
+            }
+             case 6: {
+            for (int i = 0; i < profesorCount; i++) {
+                std::cout << "\n[" << i << "] ";
+                Teachers[i]->mostrarInformacion();
+            }
+            break;
+            }
+
+            case 7: {
+            for (int i = 0; i < cursoCount; i++) {
+                std::cout << "\n[" << i << "] ";
+                Courses[i]->mostrarInformacion();
+            }
+            break;
+            }
+
+            case 8: {
+            for (int i = 0; i < matriculaCount; i++) {
+                std::cout << "\n[" << i << "] ";
+                Tuitions[i]->mostrarInformacion();
+            }
+            break;
+            }
+
+            case 9:
+            std::cout << "Saliendo del sistema...\n";
+            break;
+
+            default:
+            std::cout << "Opcion invalida.\n";
+            break;
         }
 
     } while(opcion != 9);
