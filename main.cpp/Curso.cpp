e88ea5fd3126c9993ec502aa91a154007763dc15
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 19:36:02 -0500

    commit Curso.cpp inicial

---
+++ main.cpp/Curso.cpp
@@ -0,0 +1,8 @@
+#include "../include/Curso.h"
+
+Course::Course(int coursecode,std::string coursename, int credits){
+
+    this->coursecode = coursecode;
+    this->coursename = coursename;
+    this->credits = credits;
+}
