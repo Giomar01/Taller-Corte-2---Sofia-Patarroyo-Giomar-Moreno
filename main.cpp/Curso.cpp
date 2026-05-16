9547ad0d76e6e5b98f3721a87fd3080225c54b3c
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 19:49:20 -0500

    commit Curso final

--- main.cpp/Curso.cpp
+++ main.cpp/Curso.cpp
@@ -6,3 +6,40 @@ Course::Course(int coursecode,std::string coursename, int credits){
     this->coursename = coursename;
     this->credits = credits;
 }
+int Course::getCoursecode(){
+
+    return coursecode;
+}
+
+std::string Course::getCoursename(){
+
+    return coursename;
+}
+
+int Course::getCredits(){
+
+    return credits;
+}
+
+void Course::setCoursecode(int coursecode){
+
+    this->coursecode = coursecode;
+}
+
+void Course::setCoursename(std::string coursename){
+
+    this->coursename = coursename;
+}
+
+void Course::setCredits(int credits){
+
+    this->credits = credits;
+}
+void Course::mostrarInformacion(){
+
+    std::cout << "\nINFORMACION DEL CURSO " << std::endl;
+
+    std::cout << "Codigo del curso: " << coursecode << std::endl;
+    std::cout << "Nombre del curso: " << coursename << std::endl;
+    std::cout << "Creditos: " << credits << std::endl;
+}
