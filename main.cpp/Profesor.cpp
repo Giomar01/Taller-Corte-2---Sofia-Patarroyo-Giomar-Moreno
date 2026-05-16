417d979e99abcf5568cb7a532a603ab5bfc790c5
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-15 19:51:06 -0500

    commit profesor.cpp final

--- main.cpp/Profesor.cpp
+++ main.cpp/Profesor.cpp
@@ -8,3 +8,37 @@ Teacher::Teacher(std::string Name, std::string Lastname,int Age, int Document, s
 }
 
 //setters
+void Teacher::setSpecialty(std::string Specialty){
+    this->Specialty=Specialty;
+}
+void Teacher::setSalary(double salary){
+    this->Salary=salary;
+}
+
+// Getters
+std::string Teacher::getSpecialty(){
+    return Specialty;
+}
+
+double Teacher::getSalary(){
+    return Salary;
+}
+// Metodo propio
+void Teacher::asignarCurso(){
+
+    std::cout << "Curso asignado correctamente" << std::endl;
+}
+
+// Metodo sobrescrito
+void Teacher::mostrarInformacion(){
+
+    std::cout << "===== PROFESOR =====" << std::endl;
+
+    std::cout << "Nombre: " << getname() << std::endl;
+    std::cout << "Apellido: " << getLastname() << std::endl;
+    std::cout << "Edad: " << getAge() << std::endl;
+    std::cout << "Documento: " << getDocument() << std::endl;
+
+    std::cout << "Especialidad: " << Specialty << std::endl;
+    std::cout << "Salario: " << Salary << std::endl;
+}
